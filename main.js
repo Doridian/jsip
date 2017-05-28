55dbf7f5b928c4ec7f95655e22334d69732a54b8
Mark Dietzer <mriq91@gmail.com>
2017-05-28 23:04:08 +0200

    basics for frag

--- main.js
+++ main.js
@@ -1,9 +1,101 @@
+let ourIp, serverIp, ws;
+
+function handlePacket(ipHdr, data) {
+	switch (ipHdr.protocol) {
+		case 1: // ICMP
+			const icmpPkt = ICMPPkt.fromPacket(data, 0, data.byteLength);
+			switch (icmpPkt.type) {
+				case 8: // PING
+					let offset = 0;
+					const replyIp = new IPHdr();
+					replyIp.df = true;
+					replyIp.protocol = 1;
+					replyIp.saddr = ourIp;
+					replyIp.daddr = ipHdr.saddr;
+
+					const replyICMP = new ICMPPkt();
+					replyICMP.type = 0;
+					replyICMP.code = 0;
+					replyICMP.rest = icmpPkt.rest;
+					replyICMP.data = icmpPkt.data;
+
+					replyIp.setContentLength(icmpPkt.getFullLength());
+
+					const reply = new ArrayBuffer(replyIp.getFullLength());
+
+					offset += replyIp.toPacket(reply, offset);
+					offset += replyICMP.toPacket(reply, offset);
+
+					ws.send(reply);
+					break;
+				default:
+					console.log(`Unhandled ICMP type ${icmpPkt.type}`);
+			}
+			break;
+		case 6: // TCP
+			break;
+		case 17: // UDP
+			break;
+		default:
+			console.log(`Unhandled IP protocol ${ipHdr.protocol}`);
+	}
+}
+
+let availableData = 0;
+let buffers = [];
+
+function pump() {
+	while (availableData > 20) {
+		const ipHdr = IPHdr.fromPacket(buffers[0]);
+		if (availableData < ipHdr.getFullLength()) {
+			return;
+		}
+
+		const data = new ArrayBuffer(ipHdr.getFullLength());
+		const d8 = new Uint8Array(data);
+		let left = d8.length;
+		let offset = 0;
+		while (left > 0) {
+			const b = buffers[0];
+			const b8 = new Uint8Array(b);
+			if (b8.length >= left) {
+				for (let i = 0; i < b8.length; i++) {
+					d8[offset + i] = b8[i];
+				}
+				buffers.shift();
+				left -= b8.length;
+				offset += b8.length;
+			} else {
+				for (let i = 0; i < left; i++) {
+					d8[offset + i] = b8[i];
+				}
+				buffers[0] = b.slice(left);
+				break;
+			}
+		}
+
+		availableData -= d8.length;
+
+		if (!ipHdr.daddr.equals(ourIp)) {
+			console.log(`Discarding packet not meant for us, but for ${ipHdr.daddr.toString()}`);
+			return;
+		}
+
+		if (ipHdr.mf || ipHdr.frag_offset > 0) {
+			console.log(`Discarding packet that has fragmentation`);
+			return;
+		}
+
+		const pktData = data.slice(ipHdr.getContentOffset());
+
+		handlePacket(ipHdr, pktData);
+	}
+}
+
 function main() {
-	const ws = new WebSocket('wss://tun.doridian.net');
+	ws = new WebSocket('wss://tun.doridian.net');
 	ws.binaryType = 'arraybuffer';
 	window.ws = ws;
-
-	let ourIp, serverIp;
 
 	ws.onmessage = function(msg) {
 		const data = msg.data;
@@ -14,40 +106,9 @@ function main() {
 			console.log(`Our IP: ${ourIp}`);
 			console.log(`Server IP: ${serverIp}`);
 		} else {
-			const ipHdr = IPHdr.fromPacket(data);
-			if (!ipHdr.daddr.equals(ourIp)) {
-				console.log('Discarding packet not meant for us');
-				return;
-			}
-			switch (ipHdr.protocol) {
-				case 1: // ICMP
-					const icmpPkt = ICMPPkt.fromPacket(data, ipHdr.getContentOffset(), ipHdr.getContentLength());
-					switch (icmpPkt.type) {
-						case 8: // PING
-							let offset = 0;
-							const replyIp = new IPHdr();
-							replyIp.df = true;
-							replyIp.protocol = 1;
-							replyIp.saddr = ourIp;
-							replyIp.daddr = ipHdr.saddr;
-
-							const replyICMP = new ICMPPkt();
-							replyICMP.type = 0;
-							replyICMP.code = 0;
-							replyICMP.rest = icmpPkt.rest;
-							replyICMP.data = icmpPkt.data;
-
-							replyIp.setContentLength(icmpPkt.getFullLength());
-
-							const reply = new ArrayBuffer(replyIp.getFullLength());
-
-							offset += replyIp.toPacket(reply, offset);
-							offset += replyICMP.toPacket(reply, offset);
-
-							ws.send(reply);
-							break;
-					}
-			}
+			buffers.push(data);
+			availableData += data.byteLength;
+			pump();
 		}
 	}
 }
