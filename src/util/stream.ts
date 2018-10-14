75f90375bb957bdb4e9fba5eac5c0b18f537aa5d
Mark Dietzer <mriq91@gmail.com>
2018-10-14 15:57:27 +0200

    stream stuff

---
+++ src/util/stream.ts
@@ -0,0 +1,81 @@
+export class StreamNotEnoughDataError extends Error {
+
+}
+
+type ParseFunction<T> = (self: CheckpointStream<T>, state?: T) => T | undefined;
+
+// tslint:disable-next-line:max-classes-per-file
+export class CheckpointStream<T> {
+    private data: Uint8Array[] = [];
+    private len: number = 0;
+    private state: T;
+    private parseFunc: ParseFunction<T>;
+    private lastReadDelim = -1;
+    private lastReadEnd = -1;
+    private lastStartPosOffset = 0;
+
+    constructor(parseFunc: ParseFunction<T>, defaultState: T) {
+        this.parseFunc = parseFunc;
+        this.state = defaultState;
+    }
+
+    public add(data: Uint8Array) {
+        this.data.push(data);
+        this.len += data.length;
+
+        const res = this.parseFunc(this, this.state);
+        if (res !== undefined) {
+            this.state = res;
+        }
+    }
+
+    public readUntil(delim: number) {
+        let startPosOffset = (this.lastReadDelim === delim) ? this.lastStartPosOffset : 0;
+
+        this.lastReadDelim = delim;
+
+        for (let i = (this.lastReadDelim === delim) ? this.lastReadEnd : 0; i < this.data.length; i++) {
+            const data = this.data[i];
+
+            const startPos = data.findIndex((elem) => elem === delim);
+            if (startPos >= 0) {
+                return this.read(startPosOffset + startPos + 1);
+            }
+
+            startPosOffset += data.length;
+        }
+
+        this.lastStartPosOffset = startPosOffset;
+        this.lastReadEnd = this.data.length;
+
+        throw new StreamNotEnoughDataError();
+    }
+
+    public read(len: number) {
+        if (len > this.len) {
+            throw new StreamNotEnoughDataError();
+        }
+
+        this.lastReadDelim = -1; // Invalidate state of readUntil
+
+        const res = new Uint8Array(len);
+        let dataLeft = len;
+        let pos = 0;
+        while (dataLeft > 0) {
+            const d = this.data[0];
+            if (d.byteLength > dataLeft) {
+                res.set(new Uint8Array(d.buffer, d.byteOffset, dataLeft), pos);
+                this.data[0] = new Uint8Array(d.buffer, d.byteOffset + dataLeft, d.byteLength - dataLeft);
+                break;
+            }
+            res.set(d, pos);
+            this.data.shift();
+            dataLeft -= d.byteLength;
+            pos += d.byteLength;
+        }
+
+        this.len -= len;
+
+        return res;
+    }
+}
