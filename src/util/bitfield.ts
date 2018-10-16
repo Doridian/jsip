ca10860df7aaf753d64c5a5334336da7d579ad76
Mark Dietzer <mriq91@gmail.com>
2018-10-16 02:40:18 +0200

    rm unused API

--- src/util/bitfield.ts
+++ src/util/bitfield.ts
@@ -7,23 +7,6 @@ export class BitArray {
         this.pos = 0;
     }
 
-    public get(pos: number, len: number) {
-        if (len < 1 || len >= 8 || !isFinite(len)) {
-            throw new RangeError("len must be between 1 and 7 inclusive");
-        }
-
-        const byteIndex = pos >>> 3;
-        const offset = pos & 7;
-        if (offset + len > 8) {
-            throw new RangeError(`Cannot read accross byte boundaries; ${offset}; ${len}; ${pos}`);
-        }
-
-        const curData = this.data[byteIndex];
-        const mask = (1 << len) - 1;
-        const shift = 8 - offset - len;
-        return (curData >> shift) & mask;
-    }
-
     public skip(len: number) {
         this.pos += len;
     }
@@ -38,7 +21,20 @@ export class BitArray {
         return this.read(1) !== 0;
     }
 
-    public reset() {
-        this.pos = 0;
+    private get(pos: number, len: number) {
+        if (len < 1 || len >= 8 || !isFinite(len)) {
+            throw new RangeError("len must be between 1 and 7 inclusive");
+        }
+
+        const byteIndex = pos >>> 3;
+        const offset = pos & 7;
+        if (offset + len > 8) {
+            throw new RangeError(`Cannot read accross byte boundaries; ${offset}; ${len}; ${pos}`);
+        }
+
+        const curData = this.data[byteIndex];
+        const mask = (1 << len) - 1;
+        const shift = 8 - offset - len;
+        return (curData >> shift) & mask;
     }
 }
